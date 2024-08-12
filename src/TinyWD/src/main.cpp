d8c0532f0308fcf3f5ba61580fa4d3eada724d04
Wilfried Klaas <w.klaas@gmx.de>
2024-08-12 14:38:42 +0200

    start iplementing wd code
    adding SMD variant to pcb

--- src/TinyWD/src/main.cpp
+++ src/TinyWD/src/main.cpp
@@ -1,18 +1,45 @@
 #include <Arduino.h>
+#include <SimpleTimer.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int wdPin = 0; // PB0, PB1, PB2
+const int resetPin = 3; //PB3
+const int ledPin = 4; //PB4
+
+int wd_timer_id;
+int currState = HIGH;
+int prevState = LOW;
+
+// Create timer instances
+SimpleTimer timer;
+
+void wdCallback();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(resetPin, OUTPUT);
+  digitalWrite(resetPin, HIGH);
+  wd_timer_id = timer.setInterval(30000, wdCallback);
+}
+
+void ledHeartbeat(void) {
+  digitalWrite(ledPin, currState = digitalRead(wdPin));
+  if ( currState != prevState) { // State has changed
+    prevState = currState; // Update
+    timer.restartTimer(wd_timer_id);
+  }
+}
+
+void resetHost(int resetPin) {
+  digitalWrite(resetPin, LOW); 
+  delay(5000); 
+  digitalWrite(resetPin, HIGH);
+}
+
+void wdCallback() {
+  resetHost(resetPin); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  timer.run();
+  ledHeartbeat();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
